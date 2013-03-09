1e9e06aae1b7fd6792dd2d313ec02917e26ad95e
Yaroslav.Gavrilovich <yaroslavlancelot@gmail.com>
2013-03-09 20:20:30 +0200

    added physics AndEngine extension

--- SpaceInvadors/src/com/gmail/yaroslavlancelot/GameActivity.java
+++ SpaceInvadors/src/com/gmail/yaroslavlancelot/GameActivity.java
@@ -6,56 +6,46 @@ import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.entity.util.FPSLogger;
-import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.ui.activity.BaseGameActivity;
 
 /**
  * Main game Activity.
  */
-public class GameActivity extends SimpleBaseGameActivity {
-    // ===========================================================
-    // Constants
-    // ===========================================================
-    public static final int CAMERA_WIDTH = 1024;
-    public static final int CAMERA_HEIGHT = 600;
-
-    // ===========================================================
-    // Fields
-    // ===========================================================
-
-    private Camera mCamera;
-    private Scene mMainScene;
-
-    // ===========================================================
-    // Constructors
-    // ===========================================================
-
-    // ===========================================================
-    // Getter & Setter
-    // ===========================================================
-
-    // ===========================================================
-    // Methods for/from SuperClass/Interfaces
-    // ===========================================================
+public class GameActivity extends BaseGameActivity {
+    private static final int sCameraWidth = 1600;
+    private static final int sCameraHeight = 1000;
+    private ITextureRegion mPlayerTextureRegion;
 
     @Override
     public EngineOptions onCreateEngineOptions() {
-        mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
+        Camera camera = new Camera(0, 0, sCameraWidth, sCameraHeight);
+        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(sCameraWidth, sCameraHeight), camera);
     }
 
     @Override
-    protected void onCreateResources() {
+    public void onCreateResources(OnCreateResourcesCallback onCreateResourcesCallback) throws Exception {
+        BitmapTextureAtlas playerTexture = new BitmapTextureAtlas(getTextureManager(), 64, 64);
+        mPlayerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(playerTexture, this, "icon.png", 0, 0);
+        playerTexture.load();
+        onCreateResourcesCallback.onCreateResourcesFinished();
     }
-
 
     @Override
-    protected Scene onCreateScene() {
-        engine.registerUpdateHandler(new FPSLogger());
-        mMainScene = new Scene();
-        mMainScene.setBackground(new Background(1, 1, 1));
-
-        return mMainScene;
+    public void onCreateScene(OnCreateSceneCallback onCreateSceneCallback) throws Exception {
+        Scene scene = new Scene();
+        scene.setBackground(new Background(0, 125, 58));
+        onCreateSceneCallback.onCreateSceneFinished(scene);
     }
 
+    @Override
+    public void onPopulateScene(Scene scene, OnPopulateSceneCallback onPopulateSceneCallback) throws Exception {
+        Sprite player = new Sprite(sCameraWidth / 2, sCameraHeight / 2, mPlayerTextureRegion, mEngine.getVertexBufferObjectManager());
+        player.setRotation(45.f);
+        scene.attachChild(player);
+        onPopulateSceneCallback.onPopulateSceneFinished();
+    }
 }
