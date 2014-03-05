127a6e8a01a7bfe4284bd4a7f02fab30b4491418
Yaroslav Havrylovych <yaroslavlancelot@gmail.com>
2014-03-05 21:55:55 +0200

    deleted unused stuff

--- SpaceInvaders/src/main/java/com/gmail/yaroslavlancelot/spaceinvaders/activities/ingame/SinglePlayerGameActivity.java
+++ SpaceInvaders/src/main/java/com/gmail/yaroslavlancelot/spaceinvaders/activities/ingame/SinglePlayerGameActivity.java
@@ -1,30 +1,7 @@
 package com.gmail.yaroslavlancelot.spaceinvaders.activities.ingame;
 
-import com.gmail.yaroslavlancelot.spaceinvaders.network.GameSocketServer;
-import com.gmail.yaroslavlancelot.spaceinvaders.network.callbacks.client.InGame;
-import org.andengine.engine.options.EngineOptions;
-
 /**
  * Single player game
  */
-public class SinglePlayerGameActivity extends PhysicWorldGameActivity implements InGame {
-    private GameSocketServer mGameSocketServer;
-
-    @Override
-    public EngineOptions onCreateEngineOptions() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                mGameSocketServer = GameSocketServer.getGameSocketServer();
-                mGameSocketServer.addInGameCallbacks(SinglePlayerGameActivity.this);
-            }
-        });
-        return super.onCreateEngineOptions();
-    }
-
-    @Override
-    public void newBuildingCreate(int buildingId) {
-        if (mBlueTeam != null && mBlueTeam.getTeamPlanet() != null)
-            mBlueTeam.getTeamPlanet().createBuildingById(buildingId);
-    }
+public class SinglePlayerGameActivity extends PhysicWorldGameActivity {
 }
