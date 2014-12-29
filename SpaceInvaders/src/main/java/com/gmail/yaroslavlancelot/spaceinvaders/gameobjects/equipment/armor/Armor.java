acb5bce1192b372396a94fdc01c06987161dd645
Yaroslav Havrylovych <yaroslav.gavrilovich@mobenga.com>
2014-12-30 00:24:58 +0200

    issue-98 : changed calculating distance for attack logic
    Also rotate unit to target center (before it was corner) before attack
    
    Also fix NullPointer in Damage because of not final variable

--- SpaceInvaders/src/main/java/com/gmail/yaroslavlancelot/spaceinvaders/gameobjects/equipment/armor/Armor.java
+++ SpaceInvaders/src/main/java/com/gmail/yaroslavlancelot/spaceinvaders/gameobjects/equipment/armor/Armor.java
@@ -7,29 +7,20 @@ import com.gmail.yaroslavlancelot.spaceinvaders.gameobjects.equipment.weapons.Da
  * and {@link com.gmail.yaroslavlancelot.spaceinvaders.gameobjects.equipment.weapons.Damage.DamageType}.
  */
 public class Armor {
-    private static float[][] sArmorSafetyTable;
+    private static final float[][] sArmorSafetyTable = new float[][]{
+            new float[]{1.5f, .85f, 1f, 1.2f, .45f, .45f},
+            new float[]{1.2f, .9f, 1.2f, .7f, 1f, .7f},
+            new float[]{1f, 1.05f, 1.4f, .6f, .95f, .6f},
+            new float[]{1f, 1f, 1f, 1f, 1f, 1f},
+            new float[]{1f, 1.1f, .75f, .95f, 1.2f, .75f},
+            new float[]{.3f, .5f, .45f, .6f, .5f, 1.75f}
+    };
     private int mArmorValue;
     private ArmorType mArmorType;
 
     public Armor(String armorType, final int armorValue) {
         mArmorValue = armorValue;
         mArmorType = ArmorType.valueOf(armorType.toUpperCase());
-    }
-
-    public static void initSafetyTable() {
-        sArmorSafetyTable = new float[Damage.DamageType.values().length][];
-        //RAILGUN
-        sArmorSafetyTable[0] = new float[]{1.5f, .85f, 1f, 1.2f, .45f, .45f};
-        //LASER
-        sArmorSafetyTable[1] = new float[]{1.2f, .9f, 1.2f, .7f, 1f, .7f};
-        //Neutrino blaster
-        sArmorSafetyTable[2] = new float[]{1f, 1.05f, 1.4f, .6f, .95f, .6f};
-        //Higgson
-        sArmorSafetyTable[3] = new float[]{1f, 1f, 1f, 1f, 1f, 1f};
-        //Annihilator
-        sArmorSafetyTable[4] = new float[]{1f, 1.1f, .75f, .95f, 1.2f, .75f};
-        //Quaker
-        sArmorSafetyTable[3] = new float[]{.3f, .5f, .45f, .6f, .5f, 1.75f};
     }
 
     public int getDamage(Damage damage) {
