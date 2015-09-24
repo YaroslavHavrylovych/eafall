42e2d0c16717b129d3c3e1b26f4f87e10b9c0cdd
Yaroslav Havrylovych <yaroslav.havrylovych@gmail.com>
2015-09-24 13:33:57 +0300

    issue-154: wealth and special buildings description updated

--- eafall/src/main/java/com/gmail/yaroslavlancelot/eafall/game/popup/rolling/description/updater/building/wealth/DescriptionAreaUpdater.java
+++ eafall/src/main/java/com/gmail/yaroslavlancelot/eafall/game/popup/rolling/description/updater/building/wealth/DescriptionAreaUpdater.java
@@ -6,8 +6,8 @@ import com.gmail.yaroslavlancelot.eafall.game.alliance.AllianceHolder;
 import com.gmail.yaroslavlancelot.eafall.game.alliance.IAlliance;
 import com.gmail.yaroslavlancelot.eafall.game.entity.gameobject.building.BuildingId;
 import com.gmail.yaroslavlancelot.eafall.game.entity.gameobject.building.dummy.WealthBuildingDummy;
-import com.gmail.yaroslavlancelot.eafall.game.visual.text.DescriptionText;
 import com.gmail.yaroslavlancelot.eafall.game.popup.rolling.description.updater.BaseDescriptionAreaUpdater;
+import com.gmail.yaroslavlancelot.eafall.game.visual.text.DescriptionText;
 
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.shape.Shape;
@@ -28,14 +28,7 @@ public class DescriptionAreaUpdater extends BaseDescriptionAreaUpdater {
         Text text = createDescriptionText(0, R.string.description_cost, vertexBufferObjectManager);
         mCost = createDescriptionText(text.getWidth() + mSpace, text.getY(), "350", vertexBufferObjectManager);
         // produce
-        text = createDescriptionText(1, R.string.description_description, vertexBufferObjectManager);
-        mDescription = createDescriptionText(text.getWidth() + mSpace, text.getY(),
-                EaFallApplication.getContext().getString(R.string.reptile_city_description), vertexBufferObjectManager);
-    }
-
-    @Override
-    protected void iniDescriptionTextList() {
-        mDescriptionTextList = new ArrayList<Text>(4);
+        mDescription = createDescriptionText(2, R.string.reptile_city_description, vertexBufferObjectManager);
     }
 
     @Override
@@ -48,5 +41,10 @@ public class DescriptionAreaUpdater extends BaseDescriptionAreaUpdater {
         mCost.setText(Integer.toString(dummy.getCost(buildingId.getUpgrade())));
         //description
         mDescription.setText(EaFallApplication.getContext().getString(dummy.getDescriptionStringId()));
+    }
+
+    @Override
+    protected void iniDescriptionTextList() {
+        mDescriptionTextList = new ArrayList<>(3);
     }
 }
