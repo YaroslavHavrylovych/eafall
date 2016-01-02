f551c5c7fbf56c852426f057d1737607ba27cfbb
Yaroslav Havrylovych <yaroslav.havrylovych@gmail.com>
2016-01-02 21:26:19 +0200

    updated sh build scripts

--- clean_and_build.sh
+++ clean_and_build.sh
@@ -1,2 +1,22 @@
-./gradlew :eafall:clean
-./gradlew :eafall:assembleDemoDebug --no-rebuild
+#!/bin/bash
+DEMO="Demo";
+DEV="Dev";
+
+build_variant=$1
+
+if [ "$build_variant" = "" ]; then
+   build_variant=$DEMO 
+else 
+    if [ "$build_variant" = "1" ]; then
+        build_variant=$DEV
+    else 
+        if [ "$build_variant" = "2" ]; then
+            build_variant=$DEMO
+        fi
+    fi
+fi
+
+result=":eafall:assemble${build_variant}Debug"
+
+eval "./gradlew :eafall:clean"
+eval "./gradlew ${result} --no-rebuild"
--- rebuild_run.sh
+++ rebuild_run.sh
@@ -1,2 +1,5 @@
-./clean_and_build.sh
-./run_on_device.sh
+#!/bin/bash
+val=$1
+
+eval "./clean_and_build.sh $val"
+eval "./run_on_device.sh"
